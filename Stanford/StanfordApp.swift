433dc4ed31018def8aa5f1f438359b486e8fca31
AlexRozh <alexrozh1988@gmail.com>
2021-07-04 21:01:16 +0300

    Initial Commit

---
+++ Stanford/StanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordApp.swift
+//  Stanford
+//
+//  Created by Alexander Rozhdestvenskiy on 04.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct StanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
